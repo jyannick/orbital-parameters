74f915248df045c5e1c35641d6ee3e61c1ea3983
Yannick Jeandroz <yannick.jeandroz@gmail.com>
2020-12-10 12:08:42 +0100

    compute satellite position properly (fix #2)

--- callback.js
+++ callback.js
@@ -6,17 +6,16 @@ const a = sma.value,
   Gomega = (raan.value * window.Math.PI) / 180,
   mu = 3.986004418e14,
   earth_radius = 6378,
-  anomaly_index = window.Math.round((anomaly.value / 360) * N),
-  x_shape = orbit_shape.data["x"],
-  y_shape = orbit_shape.data["y"],
+  x_shape_array = orbit_shape.data["x"],
+  y_shape_array = orbit_shape.data["y"],
   x_sat_in_orbital_plane = position_in_orbital_plane.data["x"],
   y_sat_in_orbital_plane = position_in_orbital_plane.data["y"],
   x_sat = position_3d.data["x"],
   y_sat = position_3d.data["y"],
   z_sat = position_3d.data["z"],
-  x = orbit_3d.data["x"],
-  y = orbit_3d.data["y"],
-  z = orbit_3d.data["z"],
+  x_array = orbit_3d.data["x"],
+  y_array = orbit_3d.data["y"],
+  z_array = orbit_3d.data["z"],
   x_apsides_in_orbital_plane = apsides_in_orbital_plane.data["x"],
   y_apsides_in_orbital_plane = apsides_in_orbital_plane.data["y"];
 
@@ -27,22 +26,38 @@ let sin_Gomega = window.Math.sin(Gomega);
 let cos_i = window.Math.cos(inc);
 let sin_i = window.Math.sin(inc);
 
-for (let i = 0; i < N; i++) {
-  let v = (i / (N - 1)) * 2 * window.Math.PI;
+function compute_orbit(v) {
   let cos_v = window.Math.cos(v);
   let sin_v = window.Math.sin(v);
   let r = p / (1 + e * cos_v);
 
-  x_shape[i] = r * cos_omega * cos_v - r * sin_omega * sin_v;
-  y_shape[i] = r * sin_omega * cos_v + r * cos_omega * sin_v;
+  let x_shape = r * cos_omega * cos_v - r * sin_omega * sin_v;
+  let y_shape = r * sin_omega * cos_v + r * cos_omega * sin_v;
 
-  let x_incli_only = -x_shape[i];
-  let y_incli_only = -y_shape[i] * cos_i;
-  let z_incli_only = y_shape[i] * sin_i;
+  let x_incli_only = -x_shape;
+  let y_incli_only = -y_shape * cos_i;
+  let z_incli_only = y_shape * sin_i;
 
-  x[i] = x_incli_only * cos_Gomega - y_incli_only * sin_Gomega;
-  y[i] = x_incli_only * sin_Gomega + y_incli_only * cos_Gomega;
-  z[i] = z_incli_only;
+  let x = x_incli_only * cos_Gomega - y_incli_only * sin_Gomega;
+  let y = x_incli_only * sin_Gomega + y_incli_only * cos_Gomega;
+  let z = z_incli_only;
+  return {
+    x_shape,
+    y_shape,
+    x,
+    y,
+    z,
+  };
+}
+
+for (let i = 0; i < N; i++) {
+  let v = (i / (N - 1)) * 2 * window.Math.PI;
+  let results = compute_orbit(v);
+  x_shape_array[i] = results.x_shape;
+  y_shape_array[i] = results.y_shape;
+  x_array[i] = results.x;
+  y_array[i] = results.y;
+  z_array[i] = results.z;
 }
 
 x_apsides_in_orbital_plane[0] = a * (1 - e) * cos_omega;
@@ -50,11 +65,12 @@ y_apsides_in_orbital_plane[0] = a * (1 - e) * sin_omega;
 x_apsides_in_orbital_plane[1] = -a * (1 + e) * cos_omega;
 y_apsides_in_orbital_plane[1] = -a * (1 + e) * sin_omega;
 
-x_sat_in_orbital_plane[0] = x_shape[anomaly_index];
-y_sat_in_orbital_plane[0] = y_shape[anomaly_index];
-x_sat[0] = x[anomaly_index];
-y_sat[0] = y[anomaly_index];
-z_sat[0] = z[anomaly_index];
+let sat_results = compute_orbit((anomaly.value * window.Math.PI) / 180);
+x_sat_in_orbital_plane[0] = sat_results.x_shape;
+y_sat_in_orbital_plane[0] = sat_results.y_shape;
+x_sat[0] = sat_results.x;
+y_sat[0] = sat_results.y;
+z_sat[0] = sat_results.z;
 
 orbit_shape.change.emit();
 orbit_3d.change.emit();
